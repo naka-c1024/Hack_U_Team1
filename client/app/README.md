700beb6ce5226582a9db2b37f9c9aec06fca26ac
ibuibukiki <63579269+ibuibukiki@users.noreply.github.com>
2024-06-14 18:22:16 +0900

    Client/setup (#41)
    * add: .gitignoreを追加
    
    * add: template 追加
    
    * remove: 不要なコメントの削除
    
    * update: riverpod+hooks導入
    
    * add: google fonts 導入
    
    * update: フォルダ構成をREADMEに記載

---
+++ client/app/README.md
@@ -0,0 +1,16 @@
+# app
+
+Hack U Team 1 Project !!
+
+## フォルダ構成 (仮)
+- app/lib/
+    - main.dart
+    - Domain : データモデルを管理
+    - Usecases : API関連の処理を管理
+    - Views : 画面の情報を管理
+
+## 使用したパッケージ
+- google fonts : https://fonts.google.com/?subset=japanese&noto.script=Hira
+- hooks_riverpod : https://pub.dev/packages/hooks_riverpod
+
+
---
+++ client/app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hack U team 1',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+          fontFamily: 'Noto Sans JP',
+      ),
+      home: Container(),
+    );
+  }
+}
