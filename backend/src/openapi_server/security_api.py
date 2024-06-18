4dbfa0ea124f32f59b7c4979e0590d29c70d376a
ShimadaTakutoLuke <smd0223takuto@gmail.com>
2024-06-19 02:11:35 +0900

    add: signup, login機能実装

--- backend/src/openapi_server/security_api.py
+++ backend/src/openapi_server/security_api.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from typing import List
+from typing import List, Tuple, Optional  # noqa: F401
 
 from fastapi import Depends, Security  # noqa: F401
 from fastapi.openapi.models import OAuthFlowImplicit, OAuthFlows  # noqa: F401
@@ -18,3 +18,34 @@ from fastapi.security.api_key import APIKeyCookie, APIKeyHeader, APIKeyQuery  # 
 
 from openapi_server.models.extra_models import TokenModel
 
+from passlib.context import CryptContext
+
+from sqlalchemy.engine import Result
+from sqlalchemy import select
+
+from openapi_server.db_model.tables import Users
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+async def authenticate_user(db, username: str, password: str):
+    result: Result = await (
+        db.execute(
+            select(
+                Users
+            ).where(
+                Users.username == username
+            )
+        )
+    )
+    user: Optional[Tuple[Users]] = result.first()
+    if not user:
+        return False
+    if not verify_password(password, user[0].password_hash):
+        return False
+    return user[0]
