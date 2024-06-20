cb0699ff906fd9aa38d90a2d4048da843ae35360
ibuibukiki <rocoroco523@gmail.com>
2024-06-20 11:04:46 +0900

    update: ユーザー周りのAPIテストを実装

--- client/app/test/api_test.dart
+++ client/app/test/api_test.dart
@@ -1,10 +1,60 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Test: API is Running',() async {
-      var url = Uri.parse('http://localhost:8080/ok');
-      var response = await http.get(url);
+  test('Test: API is running', () async {
+    var url = Uri.parse('http://localhost:8080/ok');
+    var response = await http.get(url);
+    expect(response.statusCode, 200);
+  });
+
+  group('User Account Test', () {
+    final userName = DateTime.now().toString();
+    test('Test: User registered successfully', () async {
+      var url = Uri.parse('http://localhost:8080/sign_up');
+      var headers = {'Content-Type': 'application/json'};
+      var requestBody = jsonEncode({
+        'username': userName,
+        'password': 'password',
+        'area': 0,
+      });
+      var response = await http.post(url, headers: headers, body: requestBody);
       expect(response.statusCode, 200);
+    });
+    test('Test: User registered failed', () async {
+      var url = Uri.parse('http://localhost:8080/sign_up');
+      var headers = {'Content-Type': 'application/json'};
+      var requestBody = jsonEncode({
+        'username': userName,
+        'password': 'password',
+        'area': 0,
+      });
+      var response = await http.post(url, headers: headers, body: requestBody);
+      expect(response.statusCode, 500);
+    });
+    test('Test: User login successfully', () async {
+      var url = Uri.parse('http://localhost:8080/login');
+      var headers = {'Content-Type': 'application/json'};
+      var requestBody = jsonEncode({
+        'username': userName,
+        'password': 'password',
+      });
+      var response = await http.post(url, headers: headers, body: requestBody);
+      expect(response.statusCode, 200);
+      var jsonResponse = jsonDecode(response.body);
+      var userId = jsonResponse['userId'];
+      expect(userId,isA<int>()); // int型がt型が返ってくることを確認
+    });
+    test('Test: User login failed', () async {
+      var url = Uri.parse('http://localhost:8080/login');
+      var headers = {'Content-Type': 'application/json'};
+      var requestBody = jsonEncode({
+        'username': userName,
+        'password': 'not_password',
+      });
+      var response = await http.post(url, headers: headers, body: requestBody);
+      expect(response.statusCode, 401);
+    });
   });
 }
