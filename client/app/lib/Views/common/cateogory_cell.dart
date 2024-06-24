f84219b846a4c8e335dbdc5717f5f46eb2cb6d1a
ibuibukiki <rocoroco523@gmail.com>
2024-06-24 22:42:34 +0900

    add: カテゴリのイラストを挿入

--- client/app/lib/Views/common/cateogory_cell.dart
+++ client/app/lib/Views/common/cateogory_cell.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -24,22 +25,48 @@ class CategoryCell extends HookConsumerWidget {
         onTap: () {
           ref.read(categoryProvider.notifier).state = categoryIndex;
           if (isSheet) {
-            Navigator.of(context).pop(1);
+            Future.delayed(const Duration(milliseconds: 300), () {
+              Navigator.of(context).pop(1);
+            });
           }
         },
         child: Ink(
           width: 88,
+          padding: const EdgeInsets.only(top: 4),
           child: Column(
             children: [
-              Container(
-                height: 80,
-                width: 80,
-                decoration: BoxDecoration(
-                  color: selectedIndex == categoryIndex
-                      ? const Color(0xff717171)
-                      : const Color(0xffd9d9d9),
-                  shape: BoxShape.circle,
-                ),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 80,
+                    width: 80,
+                    decoration: BoxDecoration(
+                      color: selectedIndex == categoryIndex
+                          ? const Color(0xfff4f4f4)
+                          : Colors.transparent,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  selectedIndex == categoryIndex
+                      ? Positioned(
+                          top: 16,
+                          left: -4,
+                          child: Container(
+                            transform: Matrix4.rotationZ(-15 * pi / 180),
+                            child: Image.asset(
+                              '/Users/ibuki/StudioProjects/Hack_U_Team1/client/app/assets/images/category_$categoryIndex.png',
+                              height: 72,
+                              width: 72,
+                            ),
+                          ),
+                        )
+                      : Image.asset(
+                          '/Users/ibuki/StudioProjects/Hack_U_Team1/client/app/assets/images/category_$categoryIndex.png',
+                          height: 72,
+                          width: 72,
+                        ),
+                ],
               ),
               const SizedBox(height: 8),
               Text(
