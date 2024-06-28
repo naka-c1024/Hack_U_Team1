9e23cc4126ddab748f70cd012a29b502769f0fd4
Takuto <68723774+ShimadaTakutoLuke@users.noreply.github.com>
2024-06-28 10:02:57 +0900

    家具レコメンド・説明の生成精度向上 (#179)
    * fix: 他ファイルから参照された際にエラーがでる問題を修正
    
    * add: 家具レコメンド機能実装
    
    * delete: ダミーデータの削除
    
    * update: furniture_list_response形式を変更
    
    crud/furniture.py get_furniture_listメソッドがfurniture_list_response形式の戻り値を持つためそこに合わせる
    
    * update: get_furniture_listがcolorでの絞り込みができるように変更
    
    * add: 家具レコメンド機能実装(API)
    
    * update: リファクタリング
    
    * add: レコメンドの例を追加
    
    * add: 説明文用の例を追加
    
    * add: ランダム性を追加
    
    * fix: エラー時の処理を追加
    
    * update: 出力を安定させるように変更
    
    ときどき複数出力されるため、それらを抑える処理を追加
    
    * update: 余計なコードの削除

---
+++ backend/src/openapi_server/ai/util.py
@@ -0,0 +1,46 @@
+import json
+import base64
+from pathlib import Path
+from fastapi import UploadFile
+
+# OpenAI APIキーをロード
+def load_api_key():
+    file_path = Path(__file__).parent / 'secret.json'
+    with open(file_path, 'r') as file:
+        secrets = json.load(file)
+    return secrets['OPENAI_API_KEY']
+
+# 製品情報データをロード
+def load_product_data():
+    file_path = Path(__file__).parent / 'product_data.json'
+    with open(file_path, 'r') as file:
+        product_data = json.load(file)
+    return product_data
+
+# 画像をBase64でエンコード
+async def encode_image_to_base64(image: UploadFile):
+    image_data = await image.read()
+    return base64.b64encode(image_data).decode('utf-8')
+
+# 製品情報データから色のインデックスを取得
+def select_color_index(color: str):
+    color_list = load_product_data()['colors']
+    for i, c in enumerate(color_list):
+        if color in c or c in color:
+            return i
+    raise Exception("Color not found")
+
+# 製品情報データからカテゴリのインデックスを取得
+def select_category_index(category: str):
+    category_list = load_product_data()['categories']
+    for i, c in enumerate(category_list):
+        if category in c or c in category:
+            return i
+    raise Exception("Category not found")
+
+# few_shotプロンプティングためのデータを取得
+def get_few_shot_data():
+    file_path = Path(__file__).parent / 'few_shot_data.json'
+    with open(file_path, 'r') as file:
+        few_shot_data = json.load(file)
+    return few_shot_data
